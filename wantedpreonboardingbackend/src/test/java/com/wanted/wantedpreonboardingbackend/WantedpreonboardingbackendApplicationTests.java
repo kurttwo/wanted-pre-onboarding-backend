3d8606d5476f130a37eeeb59fe95b91ed62beb81
kuroring <qodydgus97@gmail.com>
2023-07-30 17:40:28 +0900

    #1 - main : Spring Boot 프로젝트 만들기
    spring initalizr 로 디렉토리 초기화

---
+++ wantedpreonboardingbackend/src/test/java/com/wanted/wantedpreonboardingbackend/WantedpreonboardingbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wanted.wantedpreonboardingbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WantedpreonboardingbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
